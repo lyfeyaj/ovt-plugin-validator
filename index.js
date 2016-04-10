5d33a8eec83613b12a6615d320ef3fbff9b34194
lyfeyaj <lyfeyaj@gmail.com>
2016-04-11 00:47:48 +0800

    add use strict and update package.json

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Validator = require('validator');
 
 let Ovt;
@@ -5,12 +7,12 @@ let Ovt;
 try {
   Ovt = require('ovt');
 } catch (e) {
-  console.log(`
-    You don't have 'ovt' installed, please run command:
+  console.log(`\u001b[31m
+    You don't have module 'ovt' installed, please run command:
     \`npm install ovt --save\`
     to install the missing module.
-    Otherwise 'ovt-validator' will not be activated.
-  `);
+    Otherwise 'ovt-plugin-validator' will not be activated.
+  \u001b[39m`);
 }
 
 if (Ovt) {
